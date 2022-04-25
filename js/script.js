611f5edcf3bada206a623b94bb44f21330893f88
yashbhadra <yashbhadra@gmail.com>
2022-04-25 16:22:20 +1000

    reverted the js script file

--- js/script.js
+++ js/script.js
@@ -1,43 +1,53 @@
+// ************************************************
+// Shopping Cart API
+// ************************************************
 
-var bag = (function() {
-
+var shoppingCart = (function() {
+  // =============================
+  // Private methods and propeties
+  // =============================
   cart = [];
   
+  // Constructor
   function Item(name, price, count) {
     this.name = name;
     this.price = price;
     this.count = count;
   }
   
-  // Save bag
-  function saveBag() {
-    sessionStorage.setItem('bag', JSON.stringify(cart));
+  // Save cart
+  function saveCart() {
+    sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
   }
   
-    // Load bag
-  function loadBag() {
-    cart = JSON.parse(sessionStorage.getItem('bag'));
+    // Load cart
+  function loadCart() {
+    cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
   }
-  if (sessionStorage.getItem("bag") != null) {
-    loadBag();
+  if (sessionStorage.getItem("shoppingCart") != null) {
+    loadCart();
   }
   
+
+  // =============================
+  // Public methods and propeties
+  // =============================
   var obj = {};
   
-  // Add to bag
-  obj.addToBag = function(name, price, count) {
+  // Add to cart
+  obj.addItemToCart = function(name, price, count) {
     for(var item in cart) {
       if(cart[item].name === name) {
         cart[item].count ++;
-        saveBag();
+        saveCart();
         return;
       }
     }
     var item = new Item(name, price, count);
     cart.push(item);
-    saveBag();
+    saveCart();
   }
-  // Set count from item of bag
+  // Set count from item
   obj.setCountForItem = function(name, count) {
     for(var i in cart) {
       if (cart[i].name === name) {
@@ -46,8 +56,8 @@ var bag = (function() {
       }
     }
   };
-  // Remove item from bag
-  obj.removeItemFromBag = function(name) {
+  // Remove item from cart
+  obj.removeItemFromCart = function(name) {
       for(var item in cart) {
         if(cart[item].name === name) {
           cart[item].count --;
@@ -57,27 +67,27 @@ var bag = (function() {
           break;
         }
     }
-    saveBag();
+    saveCart();
   }
 
-  // delete everything from bag
-  obj.removeItemFromBagAll = function(name) {
+  // Remove all items from cart
+  obj.removeItemFromCartAll = function(name) {
     for(var item in cart) {
       if(cart[item].name === name) {
         cart.splice(item, 1);
         break;
       }
     }
-    saveBag();
+    saveCart();
   }
 
-  // Clean bag
-  obj.clearBag = function() {
+  // Clear cart
+  obj.clearCart = function() {
     cart = [];
-    saveBag();
+    saveCart();
   }
 
-  // Count items in bag
+  // Count cart 
   obj.totalCount = function() {
     var totalCount = 0;
     for(var item in cart) {
@@ -86,31 +96,42 @@ var bag = (function() {
     return totalCount;
   }
 
-  // Bag in total
-  obj.totalBag = function() {
-    var totalBag = 0;
+  // Total cart
+  obj.totalCart = function() {
+    var totalCart = 0;
     for(var item in cart) {
-      totalBag += cart[item].price * cart[item].count;
+      totalCart += cart[item].price * cart[item].count;
     }
-    return Number(totalBag.toFixed(2));
+    return Number(totalCart.toFixed(2));
   }
 
-  // Bag List
-  obj.listBag = function() {
-    var bagCopy = [];
-    for(i in bag) {
-      item = bag[i];
+  // List cart
+  obj.listCart = function() {
+    var cartCopy = [];
+    for(i in cart) {
+      item = cart[i];
       itemCopy = {};
       for(p in item) {
         itemCopy[p] = item[p];
 
       }
       itemCopy.total = Number(item.price * item.count).toFixed(2);
-      bagCopy.push(itemCopy)
+      cartCopy.push(itemCopy)
     }
     return cartCopy;
   }
 
+  // cart : Array
+  // Item : Object/Class
+  // addItemToCart : Function
+  // removeItemFromCart : Function
+  // removeItemFromCartAll : Function
+  // clearCart : Function
+  // countCart : Function
+  // totalCart : Function
+  // listCart : Function
+  // saveCart : Function
+  // loadCart : Function
   return obj;
 })();
 
@@ -123,19 +144,19 @@ $('.add-to-cart').click(function(event) {
   event.preventDefault();
   var name = $(this).data('name');
   var price = Number($(this).data('price'));
-  bag.addToBag(name, price, 1);
+  shoppingCart.addItemToCart(name, price, 1);
   displayCart();
 });
 
 // Clear items
 $('.clear-cart').click(function() {
-  bag.clearBag();
+  shoppingCart.clearCart();
   displayCart();
 });
 
 
 function displayCart() {
-  var cartArray = bag.listBag();
+  var cartArray = shoppingCart.listCart();
   var output = "";
   for(var i in cartArray) {
     output += "<tr>"
@@ -150,15 +171,15 @@ function displayCart() {
       +  "</tr>";
   }
   $('.show-cart').html(output);
-  $('.total-cart').html(bag.totalBag());
-  $('.total-count').html(bag.totalCount());
+  $('.total-cart').html(shoppingCart.totalCart());
+  $('.total-count').html(shoppingCart.totalCount());
 }
 
 // Delete item button
 
 $('.show-cart').on("click", ".delete-item", function(event) {
   var name = $(this).data('name')
-  bag.removeItemFromBagAll(name);
+  shoppingCart.removeItemFromCartAll(name);
   displayCart();
 })
 
@@ -166,13 +187,13 @@ $('.show-cart').on("click", ".delete-item", function(event) {
 // -1
 $('.show-cart').on("click", ".minus-item", function(event) {
   var name = $(this).data('name')
-  bag.removeItemFromBag(name);
+  shoppingCart.removeItemFromCart(name);
   displayCart();
 })
 // +1
 $('.show-cart').on("click", ".plus-item", function(event) {
   var name = $(this).data('name')
-  bag.addToBag(name);
+  shoppingCart.addItemToCart(name);
   displayCart();
 })
 
@@ -180,12 +201,12 @@ $('.show-cart').on("click", ".plus-item", function(event) {
 $('.show-cart').on("change", ".item-count", function(event) {
    var name = $(this).data('name');
    var count = Number($(this).val());
-  bag.setCountForItem(name, count);
+  shoppingCart.setCountForItem(name, count);
   displayCart();
 });
 
 $('.order-now').click(function() {
-  cart = JSON.parse(sessionStorage.getItem('bag'));
+  cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
   //$.post("./cart.php",{cart:cart});
 var sendData = function() {
   $.post('./storecart.php', {
@@ -194,7 +215,7 @@ var sendData = function() {
     //console.log(response);
     alert("Order placed successfully")
     window.location.href = "http://localhost/Officewears/index.php";
-    bag.clearBag();
+    shoppingCart.clearCart();
   });
 
   
